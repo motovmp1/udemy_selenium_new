4cf0177e5b8674f3183b09c2622cd288536bee18
motovmp1 <vinicius.mpinho@gmail.com>
2020-06-15 13:07:43 +0800

    Initial commit

---
+++ pythonSelenium/demobrowser.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+import time
+
+# every browser exposes an executable file, thought selenium
+# we ned to invoke exe file such as chrome.
+driver = webdriver.Chrome(executable_path="C:\\chromedriver.exe")
+
+
+# this is allow to land inside the webapage that you need to access
+driver.get("https://rahulshettyacademy.com/")
+
+# get the website title if we inside or not
+print(driver.title)
+print(driver.current_url)
+driver.get("https://rahulshettyacademy.com/AutomationPractice/")
+print(driver.current_url)
+time.sleep(5)
+driver.back()
+driver.refresh()
+
+# driver close all browser
+time.sleep(3)
+driver.close()
