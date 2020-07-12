c174250bf8b653c7769b367f360973bfd97ffad0
motovmp1 <vinicius.mpinho@gmail.com>
2020-07-12 17:58:29 +0800

    Running 11

--- bdd_python_01.py
+++ bdd_python_01.py
@@ -10,7 +10,17 @@ print(driver.title)
 print(driver.current_url)
 driver.maximize_window()
 time.sleep(1)
+driver.set_page_load_timeout(10)
 driver.implicitly_wait(10)
 
+
 element = driver.find_element_by_xpath("//div[@class='FPdoLc tfB0Bf']//input[@name='btnI']")
+value_button = element.get_attribute('value')
 print(element.get_attribute('value'))
+
+# uma opcao seria colocar lower case
+assert "sinto-me com sorte" == value_button.lower()
+
+time.sleep(2)
+driver.close()
+
